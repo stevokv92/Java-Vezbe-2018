ad492c84d63e47310bf034316aa65ca8fada378e
stevo <stevodmitrovic@gmail.com>
2019-09-07 15:39:58 +0200

    main method added

---
+++ src/start/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package start;
+
+import form.FrmLogIn;
+/**
+ *
+ * @author stevokv
+ */
+public class Main {
+    public static void main(String[] args) {
+        FrmLogIn frmLogIn = new FrmLogIn();
+        frmLogIn.setVisible(true);
+    }
+}
